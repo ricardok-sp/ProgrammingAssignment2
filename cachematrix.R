8ec25cf9eadfd25909635d7b19e53ec1a1b85a8a
ricardok-sp <69867983+ricardok-sp@users.noreply.github.com>
2021-02-12 13:22:04 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a cache for a square matrix, storing the solve
 
-## Write a short comment describing this function
+## Creates a special "matrix" which is realy a list containing
+## functions to set/get the values / solve of a square invertible
+## matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+      x <<- y
+      s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Checks if the solve is already cached and, if it is, 
+## returns it. Otherwise, calculates the solve, stores it
+## on cache and returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
